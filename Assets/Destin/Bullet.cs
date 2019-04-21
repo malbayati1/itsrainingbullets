5e8a992259d5be60acba9fc9630793b8f3a5cbb7
Destin Trang <trangdc@uci.edu>
2019-04-20 20:57:37 -0700

    Player Movement and Shooting

---
+++ Assets/Destin/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float bulletSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBullet();
+    }
+
+
+    void MoveBullet ()
+    {
+        transform.Translate(new Vector3(0, bulletSpeed * Time.deltaTime, 0));
+    }
+}
---
+++ Assets/Destin/Shooter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : PlayerController
+{
+
+    public GameObject bullet;
+
+    bool firing = false;
+    const KeyCode FIREBUTTON = KeyCode.Mouse0;
+
+    public float fireDelay;
+    float lastFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFire();
+        CheckMovement();
+    }
+
+    void CheckFire ()
+    {
+        if (Input.GetKeyDown(FIREBUTTON))
+        {
+            firing = true;
+        }
+        else if (Input.GetKeyUp(FIREBUTTON))
+        {
+            firing = false;
+        }
+
+
+        if (firing)
+        {
+            if (lastFire >= fireDelay)
+            {
+                Fire ();
+                lastFire = 0;
+            }
+            else
+            {
+                lastFire += Time.deltaTime;
+            }
+        }
+    }
+
+
+    void Fire ()
+    {
+        Instantiate(bullet, transform.position, Quaternion.Euler(CalculateRotation()));
+    }
+
+    Vector3 CalculateRotation ()
+    {
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        Vector3 difference = Input.mousePosition - playerPos;
+        float bulletAng = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        Vector3 bulletVector = new Vector3(0, 0, bulletAng - 90);
+
+        return bulletVector;
+    }
+}
