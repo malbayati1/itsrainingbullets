f7f28e5b7a39037265ec386e0dfa88c59ac75aef
Destin Trang <trangdc@uci.edu>
2019-04-22 14:44:50 -0700

    Health Bars

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int health;
+    public int maxHealth;
+
+    public float GetHealthRatio ()
+    {
+        return ((float)health / (float)maxHealth);
+    }
+
+}
---
+++ Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarManager : MonoBehaviour
+{
+
+    public Image playerOneBar;
+    public Image playerTwoBar;
+
+    public Health playerOne;
+    public Health playerTwo;
+
+    private void Update()
+    {
+        if (playerOne != null)
+        {
+            UpdateBar(playerOneBar, playerOne);
+        }
+
+        if (playerTwo != null)
+        {
+            UpdateBar(playerTwoBar, playerTwo);
+        }
+    }
+
+    void UpdateBar (Image bar, Health player)
+    {
+        float ratio = player.GetHealthRatio();
+        //Mathf.Lerp()
+        bar.rectTransform.localScale = new Vector2(ratio, bar.rectTransform.localScale.y);
+    }
+
+}
