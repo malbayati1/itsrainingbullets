d4bff2575e150f25ac58b0a0ceec7ddce35f5361
unknown <malbayat@uci.edu>
2019-04-21 23:14:31 -0700

    Started player movement script

---
+++ Assets/Scripts/Player1 Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0, 1);
+        } 
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0, -1);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, 0);
+        }
+    }
+}
