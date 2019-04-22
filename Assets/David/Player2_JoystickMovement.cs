5fcb2408403b0c5979bf2b57e7971bfef5d271f2
David Jenkins <eelektrik1138@hotmail.com>
2019-04-22 10:04:02 -0700

    David_TestScene
    Player 2 gamepad movement and shooting, first pass
    Player 1 keyboard movement working, player 1 cannot yet shoot.

---
+++ Assets/David/Player2_JoystickMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2_JoystickMovement : MonoBehaviour
+{
+    //Player movement using the left thumbstick, axes set up for a wired Xbox 360 gamepad.  Might need minor changes for other gamepads.
+
+
+    private Rigidbody2D rb;
+    [SerializeField] protected float player_speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float directionX = Input.GetAxis("LeftThumbstickX");
+        float directionY = Input.GetAxis("LeftThumbstickY");
+        rb.velocity = new Vector2(directionX, directionY) * player_speed;
+    }
+}
---
+++ Assets/David/Player2_JoystickShooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2_JoystickShooting : MonoBehaviour
+{
+    //Player Shooting using the right thumbstick, axes set up for a Wired Xbox 360 gamepad.
+    
+
+    private int FrameCount;
+    [SerializeField] protected int frames_between_shot;
+    private bool DoShoot = false;
+    [SerializeField] protected Player2_Projectile projectile;
+    //SerializeField] protected GameObject player_location;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FrameCount = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        FrameCount++;
+        float directionX = Input.GetAxis("RightThumbstickX");
+        float directionY = Input.GetAxis("RightThumbstickY");
+        if (directionX != 0 || directionY != 0)
+        {
+            DoShoot = true;
+        }
+        if (DoShoot && FrameCount % frames_between_shot == 0)
+        {
+            Vector2 firing_vector = new Vector2(directionX, directionY);
+            Player2_Projectile new_projectile = Instantiate(projectile, firing_vector + new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        }
+        DoShoot = false;
+    }
+}
