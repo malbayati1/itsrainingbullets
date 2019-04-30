da4382a3baf176a78b304ca73fe62ad70d8c68c0
Unknown <alvizoj@uci.edu>
2019-04-29 22:05:21 -0700

    Pause menu added
    Didn't know which scene to put it in, so I added it to all of them

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool isPaused = false;
+	public GameObject pauseMenu;
+	public GameObject player;
+
+	void Start(){
+		pauseMenu.SetActive(false);
+	}
+
+    void Update() {
+    	if (Input.GetKeyDown(KeyCode.P)){
+    		if (isPaused) {
+    			pauseMenu.SetActive(false);
+        		Time.timeScale = 1f;
+        		isPaused = false;
+        	}
+        	else{	
+        		pauseMenu.SetActive(true);
+       			Time.timeScale = 0f;
+       			isPaused = true;
+        	}
+    	}
+    }
+
+    public void Resume() {
+    	pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void MainMenu() {
+    	SceneManager.LoadScene("MainMenu");
+    	Resume();
+    }
+}
