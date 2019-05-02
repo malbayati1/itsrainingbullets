fd9b1045e4483599ee48a6a80f44bb853467584c
Destin Trang <trangdc@uci.edu>
2019-05-02 01:16:12 -0700

    HP fixes

--- Assets/Scripts/HealthBarManager.cs
+++ Assets/Scripts/HealthBarManager.cs
@@ -13,7 +13,9 @@ public class HealthBarManager : MonoBehaviour
     public Health playerTwo;
 
     public float lerpSpeed;
-    float currentRatio = 1;
+
+    float ratioOne = 1;
+    float ratioTwo = 1;
 
 
     private void Start()
@@ -26,7 +28,7 @@ public class HealthBarManager : MonoBehaviour
     {
         if (playerOne != null)
         {
-            UpdateBar(playerOneBar, playerOne);
+            ratioOne = UpdateBar(playerOneBar, playerOne, ratioOne);
         } 
         else
         {
@@ -35,7 +37,7 @@ public class HealthBarManager : MonoBehaviour
 
         if (playerTwo != null)
         {
-            UpdateBar(playerTwoBar, playerTwo);
+            ratioTwo = UpdateBar(playerTwoBar, playerTwo, ratioTwo);
         }
         else
         {
@@ -43,12 +45,14 @@ public class HealthBarManager : MonoBehaviour
         }
     }
 
-    void UpdateBar (Image bar, Health player)
+    float UpdateBar (Image bar, Health player, float currentRatio)
     {
         float ratio = player.GetHealthRatio();
         ratio = Mathf.Clamp(ratio, 0, 1);
         currentRatio = Mathf.Lerp(currentRatio, ratio, Time.deltaTime * lerpSpeed);
         bar.rectTransform.localScale = new Vector2(currentRatio, bar.rectTransform.localScale.y);
+
+        return currentRatio;
     }
 
 }
