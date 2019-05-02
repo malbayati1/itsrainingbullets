7c80836c4ee772339b9c2d36605ebee4f9073805
Unknown <alvizoj@uci.edu>
2019-05-02 14:05:58 -0700

    Final build changes
    Play button takes you to "MainLevel" scene and main menu background is now blue

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void Play(){
-    	SceneManager.LoadScene("David_TestScene");
+    	SceneManager.LoadScene("MainLevel");
     }
 
     public void Quit(){
